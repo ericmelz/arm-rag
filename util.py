97530d49ca501adadb383482edf0e72a3c2201db
ericmelz <eric@emelz.com>
2023-10-29 08:42:05 -0700

    work

--- util.py
+++ util.py
@@ -1,6 +1,7 @@
-import datasets
+from datasets import load_dataset
 
 def load_gsm():
     data_path = 'data/gsm/STaR/train_rand_split.jsonl'
     dataset = load_dataset(path='json', data_files=data_path, split='train')
+    return dataset
     
